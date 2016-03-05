c3f5f17c4b3d01bdf2ac0dfc2b5b05cb6129acaf
nathants <me@nathants.com>
2016-03-04 22:58:25 -0800

    if echo is set to false, then it ignores the global override

--- shell/__init__.py
+++ shell/__init__.py
@@ -51,11 +51,11 @@ def call(*a, **kw):
 # TODO have an option for threaded runs that instead of prefixing and colorizing the output, it uses curses, and statically updates every threads stdout display. each thread gets a single line showing the current value of stdin. useful for when one threads output is drowning the others. # noqa
 # TODO add a name and color kwarg, which activate the prefixing and line coloring for multiple threaded runs. color is a bool, and iterates over a global cycle of the colors.
 # TODO actually just move all the threaded shell logic here. it doesnt belong in ec2. naming, colorizing, and keeping track of fails/successes.
-def run(*a, stream=None, echo=None, stdin='', popen=False, callback=None, warn=False, zero=False, quiet=False, raw_cmd=False, hide_stderr=False):
+def run(*a, stream=None, echo=None, stdin='', popen=False, callback=None, warn=False, zero=False, quiet=None, raw_cmd=False, hide_stderr=False):
     stream = stream or _state.get('stream') and stream is not False
     logfn = _get_logfn(stream)
     cmd = _make_cmd(a, stdin)
-    if (stream and echo is None) or echo or _state.get('echo'):
+    if (stream and echo is None) or echo or _state.get('echo') and echo is not False:
         _echo(cmd, _get_logfn(True))
     kw = {'stdout': subprocess.PIPE,
           'stderr': subprocess.DEVNULL if hide_stderr else subprocess.STDOUT,
