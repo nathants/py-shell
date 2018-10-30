5eb0203ab3e30412c240c8fbefbc9c4203651a8a
nathants <me@nathants.com>
2018-10-30 14:45:09 -0700

    test against py37

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 ### for shelling out
 
 ### installation
-`pip-3.6 install --process-dependency-links git+https://github.com/nathants/py-shell@7f141d4 # or some other git hash`
+`pip install --process-dependency-links git+https://github.com/nathants/py-shell@7f141d4 # or some other git hash`
--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setuptools.setup(
     author_email='me@nathants.com',
     url='http://github.com/nathants/py-shell',
     packages=['shell'],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=['argh >0.26, <0.27',
                       'pyyaml >3, <4',
                       'py-util'],
-    dependency_links=['https://github.com/nathants/py-util/tarball/4d1fe20ecfc0b6982933a8c9b622b1b86da2be5e#egg=py-util-0.0.1'],
+    dependency_links=['https://github.com/nathants/py-util/tarball/fa60dbf761a61beb94614af89240fd5986d26786#egg=py-util-0.0.1'],
     description='for shelling out',
 )
